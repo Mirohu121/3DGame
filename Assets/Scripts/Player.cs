e19b46dd8b3e0d77e93dd8a5f2883d6ea11b4b1a
Miwa <>
2026-07-10 10:10:46 +0900

    パッチを適応

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,14 @@ public class Player : MonoBehaviour
 
     PlayerInput playerInput;
     private float speedMax;
+     [SerializeField] float accel;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] Animator animator;
+   [SerializeField] float jumpSpeed;
+
     [SerializeField] float accel;
     Rigidbody rb;
+    Vector3 rotateTarget;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,5 +39,27 @@ public class Player : MonoBehaviour
             cameraDir * accelVec.y * accel
             + cameraRight * accelVec.x * accel;
         rb.AddForce(accelVec3D, ForceMode.Acceleration);
+
+        //プレイヤーの向きを変える
+        if(accelVec3D !=Vector3.zero)
+        {
+            rotateTarget =accelVec3D.normalized;
+        }
+
+        Vector3 forward =transform.forward;
+
+        transform.up =Vector3.up;
+
+        transform.forward =Vector3.Slerp(forward,rotateTarget,rotateSpeed*Time.deltaTime);
+
+        Vector3 velocityXZ =rb.linearVelocity;
+        velocityXZ.y =0;
+        animator.SetFloat("MoveSpeed",velocityXZ.magnitude);
+
+        if(playerInput.actions["Jump"].WasPressedThisFrame())
+        {
+            Vector3 jumpVec =new Vector3(0,jumpSpeed,0);
+            rb.AddForce(jumpVec,ForceMode.VelocityChange);
+        }
     }
 }
